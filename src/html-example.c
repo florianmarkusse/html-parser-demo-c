0e0ac78ab6eb2427c7d491ce398490acc81463fb
florianmarkusse <florianmarkusse@hotmail.com>
2023-09-16 18:46:31 +0200

    demo update

--- src/html-example.c
+++ src/html-example.c
@@ -5,41 +5,45 @@
 
 void readAndPrintHTML(const char *fileLocation) {
     // Initialize a text store to manage memory
-    TextStore textStore;
-    if (createTextStore(&textStore) != ELEMENT_SUCCESS) {
+    flo_html_TextStore textStore;
+    if (flo_html_createTextStore(&textStore) != ELEMENT_SUCCESS) {
         fprintf(stderr, "Failed to create text store!\n");
         return;
     }
 
     // Initialize a DOM structure
-    Dom dom;
-    if (createDomFromFile(fileLocation, &dom, &textStore) != DOM_SUCCESS) {
-        destroyTextStore(&textStore);
+    flo_html_Dom dom;
+    if (flo_html_createDomFromFile(fileLocation, &dom, &textStore) !=
+        DOM_SUCCESS) {
+        flo_html_destroyTextStore(&textStore);
         fprintf(stderr, "Failed to parse DOM from file!\n");
         return;
     }
 
     // Print before modification.
     printf("Before modification\n\n");
-    printHTML(&dom, &textStore);
+    flo_html_printHTML(&dom, &textStore);
 
     // Find the ID of the <body> element
-    node_id bodyNodeID = 0;
-    if (querySelector("body", &dom, &textStore, &bodyNodeID) != QUERY_SUCCESS) {
-        destroyDom(&dom);
-        destroyTextStore(&textStore);
+    flo_html_node_id bodyNodeID = 0;
+    if (flo_html_querySelector("body", &dom, &textStore, &bodyNodeID) !=
+        QUERY_SUCCESS) {
+        flo_html_destroyDom(&dom);
+        flo_html_destroyTextStore(&textStore);
         fprintf(stderr, "Failed to query DOM!\n");
         return;
     }
 
     // Check if the body element has a specific boolean property
     // In other words: "<body add-extra-p-element> ... </body>"
-    if (hasBoolProp(bodyNodeID, "add-extra-p-element", &dom, &textStore)) {
+    if (flo_html_hasBoolProp(bodyNodeID, "add-extra-p-element", &dom,
+                             &textStore)) {
         // Append HTML content to the <body> element
-        if (appendHTMLFromStringWithQuery("body", "<p>I am appended</p>", &dom,
-                                          &textStore) != DOM_SUCCESS) {
-            destroyDom(&dom);
-            destroyTextStore(&textStore);
+        if (flo_html_appendHTMLFromStringWithQuery("body",
+                                                   "<p>I am appended</p>", &dom,
+                                                   &textStore) != DOM_SUCCESS) {
+            flo_html_destroyDom(&dom);
+            flo_html_destroyTextStore(&textStore);
             fprintf(stderr, "Failed to append to DOM!\n");
             return;
         }
@@ -47,9 +51,9 @@ void readAndPrintHTML(const char *fileLocation) {
 
     // Print the modified HTML
     printf("After modification\n\n");
-    printHTML(&dom, &textStore);
+    flo_html_printHTML(&dom, &textStore);
 
     // Cleanup: Free memory and resources
-    destroyDom(&dom);
-    destroyTextStore(&textStore);
+    flo_html_destroyDom(&dom);
+    flo_html_destroyTextStore(&textStore);
 }
